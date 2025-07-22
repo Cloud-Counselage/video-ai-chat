4809a220f481be77c2e08ae5180bbce5cc22ce07
Cloud-Counselage <welcome@cloudcounselage.com>
2025-07-22 14:57:41 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("/coi-serviceworker.js");
-}
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App"
+import "../styles/index.css"
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />)
